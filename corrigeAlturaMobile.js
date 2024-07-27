da4ee61ee80c372fb9f8427b3ffc24244f0d3a49
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2024-07-27 19:31:54 -0400

    subido con alineamiento del texto del hero banner

--- corrigeAlturaMobile.js
+++ corrigeAlturaMobile.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Selecciona todos los elementos que coinciden con el selector dado
-    var elements = document.querySelectorAll('body > section.default-banner.active-blog-slider.owl-carousel.owl-theme.owl-loaded > div.owl-stage-outer > div > div.owl-item > div > div.container > div');
+    // Selecciona los elementos para aplicar height: 100vh !important;
+    var elements1 = document.querySelectorAll('body > section.default-banner.active-blog-slider.owl-carousel.owl-theme.owl-loaded > div.owl-stage-outer > div > div.owl-item > div > div.container > div');
 
-    // Itera sobre cada elemento y le añade el estilo
-    elements.forEach(function(element) {
+    elements1.forEach(function(element) {
         element.style.setProperty('height', '100vh', 'important');
+    });
+
+    // Selecciona los elementos para aplicar display: flex, flex-direction: column, justify-content: center
+    var elements2 = document.querySelectorAll('body > section.default-banner.active-blog-slider.owl-carousel.owl-theme.owl-loaded > div.owl-stage-outer > div > div.owl-item > div > div.container > div > div > div');
+
+    elements2.forEach(function(element) {
+        element.style.setProperty('display', 'flex', 'important');
+        element.style.setProperty('flex-direction', 'column', 'important');
+        element.style.setProperty('justify-content', 'center', 'important');
     });
 });
