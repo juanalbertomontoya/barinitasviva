9f4899a3628da73395f31cfa18662ce22fefbeb6
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2024-08-21 13:19:25 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Iniciar el escáner de QR automáticamente
-    const html5QrCode = new Html5Qrcode("reader");
 
+document.getElementById('scanBtn').addEventListener('click', function () {
+    let html5QrCode = new Html5Qrcode("reader");
+    
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {        
         // Insertar el valor escaneado en el input de DNI
         document.getElementById('dni').value = decodedText;
@@ -19,44 +19,49 @@ document.addEventListener("DOMContentLoaded", function() {
     .catch(err => {
         console.log("Error al iniciar el escaneo:", err);
     });
+});
 
-    // Script para la funcionalidad del popup y cookies
-    function setCookie(name, value, days) {
-        const d = new Date();
-        d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-        const expires = "expires=" + d.toUTCString();
-        document.cookie = name + "=" + value + ";" + expires + ";path=/";
-    }
+// Script para la funcionalidad del popup y cookies
 
-    function getCookie(name) {
-        const cname = name + "=";
-        const decodedCookie = decodeURIComponent(document.cookie);
-        const ca = decodedCookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(cname) === 0) {
-                return c.substring(cname.length, c.length);
-            }
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
         }
-        return "";
-    }
-
-    document.getElementById('accessBtn').addEventListener('click', function () {
-        const password = document.getElementById('passwordInput').value;
-        if (password === "8141663.23038607") {
-            setCookie("trustedDevice", "true", 365);
-            document.getElementById('passwordPopup').style.display = 'none';
-        } else {
-            alert("Contraseña incorrecta. Intente de nuevo.");
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
         }
-    });
+    }
+    return "";
+}
 
+document.getElementById('accessBtn').addEventListener('click', function () {
+    const password = document.getElementById('passwordInput').value;
+    if (password === "8141663.23038607") {
+        setCookie("trustedDevice", "true", 365);
+        document.getElementById('passwordPopup').style.display = 'none';
+    } else {
+        alert("Contraseña incorrecta. Intente de nuevo.");
+    }
+});
+
+window.onload = function() {
     if (getCookie("trustedDevice") === "true") {
         document.getElementById('passwordPopup').style.display = 'none';
     } else {
         document.getElementById('passwordPopup').style.display = 'flex';
     }
-});
+};
+
+
